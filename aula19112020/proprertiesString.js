e33a85a50dc1358ac2ee40df48d7c4dc3032211c
fcomaciel <fcomaciel@gmail.com>
2020-11-20 13:10:11 -0300

    Commit aula19112020

---
+++ aula19112020/proprertiesString.js
@@ -0,0 +1,12 @@
+// Propriedade e métodos da String
+
+// Length
+
+console.log("Isso é uma string".length);
+
+console.log("TEXTO MAIÚSCULO PARA MINÚSCULO".toLowerCase());
+
+console.log("texto maiúsculo para minúsculo".toUpperCase());
+
+console.log(" Preparando     texto planilha");
+console.log("    Preparando texto planilha     ".trim());
--- aula19112020/typeString.js
+++ aula19112020/typeString.js
@@ -1,1 +1,24 @@
+// Tipos de dados utilizado pelo JS
+
 // Strings em JS
+
+console.log('Isso é uma string');
+console.log("Isso é outra string");
+console.log(`Isso é mais uma string`);
+
+console.log("It's six o'clock");
+
+console.log("Observe nessa \"String\"");
+console.log('Observe nessa "String"');
+
+// Como imprimir caracteres especiais
+
+console.log('\'');
+console.log("\"");
+console.log("\\");
+console.log("Como imprimir \ncaracteres especiais") // \n
+console.log("Como imprimir \rcaracteres especiais") // \r
+console.log("Como imprimir \vcaracteres especiais") // \v
+console.log("Como imprimir \tcaracteres especiais") // \t
+console.log("Como imprimir \bcaracteres especiais") // \b
+
