a6be1c1c36d3408389d77bada84cc9bbf3601f9d
fcomaciel <fcomaciel@gmail.com>
2020-11-23 15:40:42 -0300

    Commit 1 23112020

---
+++ aula23112020/atibuacoes.js
@@ -0,0 +1,55 @@
+// Atibuição de valores às variáveis
+// adição
+let a = 1;
+let b = 5;
+// a + b
+console.log('Resultado de a + b:', a + b);
+
+// subtração
+let x = 50;
+let y = 15;
+// 50 - 15
+console.log('Resultado de x - y:', x - y);
+
+// multiplicação
+x = 4;
+y = 12;
+// 4 * 12
+console.log('Resultado de x * y:', x * y);
+
+/* Continuar desse ponto */
+// divisão
+x = 48;
+y = 4;
+// 48 / 4
+console.log('Resultado de x / y:', x / y);
+
+/* Ordem dos cálculo em JS */
+// 1 + 100 * 5
+let m = 1;
+let n = 100;
+let o = 5;
+console.log('Resultado de 1 + 100 * 5:', m + n * o);
+// agupando por operador
+// (1 + 100) * 5
+console.log('Resultado de (1 + 100) * 5:', (m + n) * o);
+
+// Operador de concatenação (+)
+let escola = "Senac"
+let estado = "Piauí"
+let separador = " ";
+console.log(escola + separador + estado);
+
+// Operador de módulo (%)
+x = 12;
+y = 5;
+// 12 % 5
+console.log('Resultado de x % y:', x % y);
+
+var num1 = 5;
+var num2 = 3;
+
+console.log(num1 + num2);
+
+var num2 = 5;
+console.log(num1 + num2);
---
+++ aula23112020/operadores.js
@@ -0,0 +1,12 @@
+// Operadores de atribuição composta
+
+let num1 = 5;
+let num2 = 11;
+
+// +=
+
+console.log('Resultado de num1(5) += num2(11)', num1 += num2);
+console.log('Resultado de num1(5) *= num2(11)', num1 *= num2);
+
+console.log('Alô mundo !');
+alert('Alô mundo !');
