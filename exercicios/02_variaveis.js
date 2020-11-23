cfc73db369520b42cf5df522f5d3db384f620d53
fcomaciel <fcomaciel@gmail.com>
2020-11-22 22:29:42 -0300

    Commit - exercícios

---
+++ exercicios/02_variaveis.js
@@ -0,0 +1,27 @@
+// Número (Number)
+let numero = 120;
+
+// Estringe (String)
+let texto = 'String';
+
+// Booleano (Booleans)
+const booleano = true;
+
+// Vetor (Array)
+let vetor = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Objeto (Object)
+let objeto = {
+    propriedade: 12,
+    propriedade2: 'teste'
+};
+
+// Função (Funtion)
+let umaFuncao = function () {
+    // fazer algo
+};
+
+function outraFuncao () {
+    umaFuncao();
+    umaFuncao();
+}
