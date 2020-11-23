45be5dd35f038901b6d0f7be3ab6513075a81528
fcomaciel <fcomaciel@gmail.com>
2020-11-23 12:29:52 -0300

    Commit - operadores de atribuição

---
+++ exercicios/03_operadoresAtrib.js
@@ -0,0 +1,37 @@
+// Operadores de atribuição composto
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_Operators#operador_atribuicao
+// Atribuição
+let a = 2;
+let b = 3;
+console.log(a + b);
+
+// Atribuição de adição 	a += b
+a = 7;
+b = 3;
+console.log(a += b);
+
+// Atribuição de subtração a -= b
+a = 3;
+b = 7;
+console.log(a -= b);
+
+// Atribuição de multiplicação 	a *= b
+a = 2;
+b = 13;
+console.log(a *= b);
+
+// Atribuição de divisão 	a /= b
+a = 15;
+b = 4;
+console.log(a /= b);
+
+// Atribuição de resto x %= y
+let x = 15;
+let y = 4;
+console.log(x %= y);
+
+// Atribuição exponencial 	x **= y
+x = 10;
+y = 3;
+console.log(x **= y);
+
