a47bc6d01bbd7543c1c1a4f5b5867762f08577e5
fcomaciel <fcomaciel@gmail.com>
2020-11-24 18:05:46 -0300

    Commit 2 - aula24112020

---
+++ aula25112020/respostaEexercicioVariaveis.js
@@ -0,0 +1,49 @@
+
+/*
+1.Declare uma variável de nome flor e atribua a ela o valor 'rosa'. 
+Declare uma segunda variáveis arvore e atribua a ela o valor 'ipe'.
+*/
+let flor = 'rosa', arvore = 'ipe';
+console.log(flor, arvore);
+
+// 2.Crie uma variável chamada meuNum, atribua 
+// um número inteiro a ela.Leitura Avançada
+let meuNum = -10;
+console.log(meuNum);
+
+/*
+3.Escreva um código que exiba a soma de 5 + 10, usando duas 
+variáveis: x e y com a saída demonstrando o resultado no console.
+*/
+let na = 5, nb = 10;
+console.log(na + nb) // resultado 15
+
+/*
+4.Crie uma variável chamada z, atribua x + y a ela e exiba o 
+resultado através do console.*/
+var x4 = 5, y4 = 10;
+let z4 = x4 + y4;
+console.log(z4)
+
+/*
+5.Em uma única linha, declare três variáveis com os seguintes nomes e valores:
+primeiroNome = "Juliana"
+ultimoNome = "Silva"
+idade = 35
+*/
+let primeiroNome = "Juliana", ultimoNome = "Silva", idade = 35;
+console.log(primeiroNome, ultimoNome, idade);
+
+// 6.Qual valor x tem após a execução do código a seguir?
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log(x);
+
+// 7.Qual valor x tem após a execução do código a seguir?
+let x7 = 'Paulo';
+let y7 = 'Gabriela';
+let z7 = y7;
+y7 = x7;
+x7 = z7;
+console.log(x7);
