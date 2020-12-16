49ba1ae437090b4110d9d9866523f3b74a07c455
fcomaciel <fcomaciel@gmail.com>
2020-12-16 16:35:20 -0300

    Atualização aula16122020

---
+++ aula16122020/js/script.js
@@ -0,0 +1,40 @@
+// Functions
+
+
+let p1 = prompt('Informe um valor numérico: ');
+let p2 = prompt('Informe outro valor numérico: ');
+
+function minhaFuncao(p1, p2) {
+    return p1 * p2;
+}
+document.getElementById("resultado").innerHTML = minhaFuncao(p1, p2);
+
+// ‎Função para converter graus Fahrenheit em Celsius:‎
+// Fórmula: (5/9) * (fahrenheit -32)
+
+
+let f = prompt('Qual a temperatura em Fahrenheit: ', 77);
+
+function toCelsius(f) {
+  return (5/9) * (f -32);
+}
+document.getElementById("resultado").innerHTML = toCelsius(f);
+
+// Funação sem passagem de argumento
+
+carro();
+
+function carro() {
+    var carroNome = "Fusca";
+    document.getElementById("resultado").innerHTML = typeof carroNome + " " + carroNome
+}
+document.getElementById("resultado1").innerHTML = typeof carroNome;
+
+/* EXERCÍCIOS */
+
+// 1. Criar uma função para saudação com "Alô mundo!" sem passagem de parâmetros.
+
+// 2. Criar uma função para somar 2 números passados como parâmetro.
+
+// 3. Criar uma função que pergunte seu nome e idade e escreva em parágrafos 
+// do html cada um dos dados recebidos.
---
+++ aula16122020/js/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function carro() {
+    let carroNome = "Fusca";
+    // document.getElementById("resultado").innerHTML = carroNome;
+    console.log(carroNome);
+}
